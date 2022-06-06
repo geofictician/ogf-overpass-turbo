9c393ed233688bc643becde1f11fa70bbace24d3
Luciano Jones <luciano@geofictician.net>
2022-06-06 23:27:11 +0000

    Revert "Trying to fix tileserver..."
    This reverts commit fae07aebfbb5aaf9f1e597132c112a82ffd6e4ea.

--- js/configs.js
+++ js/configs.js
@@ -5,12 +5,12 @@ export default {
     "//overpass.ogf.rent-a-planet.com/api/",
     "https://ogfoverpass.rent-a-planet.com/api/"
   ],
-  defaultTiles: "//tiles04.rent-a-planet.com/ogf-carto/{z}/{x}/{y}.png",
+  defaultTiles: "//tile.opengeofiction.net/ogf-carto/{z}/{x}/{y}.png",
   tileServerAttribution:
     "Tiles and data courtesy <a href="https://wiki.opengeofiction.net/index.php/OpenGeofiction:About">OpenGeofiction</a> and contributors.",
   suggestedTiles: [
-    "//tiles04.rent-a-planet.com/ogf-carto/{z}/{x}/{y}.png",
-    "https://tiles04.rent-a-planet.com/ogf-topo/{z}/{x}/{y}.png"
+    "//tile.opengeofiction.net/ogf-carto/{z}/{x}/{y}.png"
+    //"https://tile.opengeofiction.net/ogf-topo/{z}/{x}/{y}.png"
   ],
   defaultMapView: {
     lat: 86.716,
@@ -21,7 +21,7 @@ export default {
   short_url_service: "",
   html2canvas_use_proxy: false,
   // api key for osmnames geocoder, go to http://osmnames.org/api/ to get one if you run your own overpass instance
-  osmnamesApiKey: "gtXyh2mBSaN5zWqqqQRh",
+  osmnamesApiKey: "", // "gtXyh2mBSaN5zWqqqQRh",
   // osmAuth configuration object (used for syncing saved queries). expects an osm-auth
   //   config object (min. the oauth_consumer_key and oauth_secret must be given),
   //   see https://github.com/osmlab/osm-auth#getting-keys
